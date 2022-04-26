6d570672a8dfae1a86945109cc7b6278f04a08ef
Ye Wang <zxcsef@msn.com>
2022-04-26 14:38:56 -0400

    database settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@6zdo5yvn-@kuv*(p!#ztu!954w61_8%0or+_77l**trix!v+y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wangyex.pythonanywhere.com']
 
 
 # Application definition
