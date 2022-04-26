572ef7714c57ddb28867156e7bf592de4111b677
Ye Wang <zxcsef@msn.com>
2022-04-26 18:38:51 -0400

    New RichText Editor

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
 class Blogs(models.Model):
     title = models.CharField(max_length = 100)
+    header_image = models.ImageField(null=True, blank = True, upload_to = "images/")
     description = models.TextField()
+    body = RichTextField(blank = True, null = True)
     date = models.DateField()
 
     def __str__(self):
--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'portfolio',
+    'embed_video',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
